f18ad94f1d11725040f239929e336d5f40a8b189
Brian Birtles <birtles@gmail.com>
2020-08-25 17:19:55 +0900

    Add types for checking name entries

---
+++ src/names.ts
@@ -0,0 +1,102 @@
+import { isArrayOfStrings } from './utils';
+
+export interface NameEntryLine {
+  id: number;
+  // Kanji readings
+  k?: Array<string>;
+  // Kana readings
+  r: Array<string>;
+  tr: Array<NameTranslation>;
+}
+
+export interface NameTranslation {
+  // The type(s) for this entry. This can be missing (e.g. ノコノコ).
+  type?: Array<NameType>;
+  // The translation text itself.
+  det: Array<string>;
+  // Cross-references to other entries (in the form of an arbitrary string of
+  // Japanese text).
+  cf?: Array<string>;
+}
+
+export type NameType =
+  | 'surname'
+  | 'place'
+  | 'unclass'
+  | 'company'
+  | 'product'
+  | 'work'
+  | 'masc'
+  | 'fem'
+  | 'person'
+  | 'given'
+  | 'station'
+  | 'org'
+  | 'ok';
+
+const nameTypes: Array<NameType> = [
+  'surname',
+  'place',
+  'unclass',
+  'company',
+  'product',
+  'work',
+  'masc',
+  'fem',
+  'person',
+  'given',
+  'station',
+  'org',
+  'ok',
+];
+
+export interface NameDeletionLine {
+  id: number;
+  deleted: true;
+}
+
+export function isNameEntryLine(a: any): a is NameEntryLine {
+  return (
+    typeof a === 'object' &&
+    a !== null &&
+    // id
+    typeof a.id === 'number' &&
+    (a.id as number) > 0 &&
+    Number.isFinite(a.id) &&
+    // k
+    (typeof a.k === 'undefined' || isArrayOfStrings(a.k)) &&
+    // r
+    isArrayOfStrings(a.r) &&
+    // tr
+    Array.isArray(a.tr) &&
+    (a.tr as Array<any>).every(isNameTranslation) &&
+    // deleted (should NOT be present)
+    typeof a.deleted === 'undefined'
+  );
+}
+
+function isNameTranslation(a: any): a is NameTranslation {
+  return (
+    typeof a === 'object' &&
+    a !== null &&
+    (typeof a.type === 'undefined' ||
+      (isArrayOfStrings(a.type) &&
+        (a.type as Array<NameType>).every((type) =>
+          nameTypes.includes(type)
+        ))) &&
+    isArrayOfStrings(a.det) &&
+    (typeof a.cf === 'undefined' || isArrayOfStrings(a.cf))
+  );
+}
+
+export function isNameDeletionLine(a: any): a is NameDeletionLine {
+  return (
+    typeof a === 'object' &&
+    a !== null &&
+    typeof a.id === 'number' &&
+    (a.id as number) > 0 &&
+    Number.isFinite(a.id) &&
+    typeof a.deleted === 'boolean' &&
+    a.deleted
+  );
+}
